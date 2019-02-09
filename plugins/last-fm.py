5bc1e6be795d0d72d041ef1f5f275b7c56239fb9
koronis <nojusr@gmail.com>
2019-02-10 00:19:35 +0200

    edited to *mostly* conform to PEP8

--- plugins/last-fm.py
+++ plugins/last-fm.py
@@ -32,9 +32,10 @@ from core import db, hook
 # db table definition
 quote_columns: List[str] = [
     'irc_nick', 'lastfm_nick']
-    
+
 api_url = 'http://ws.audioscrobbler.com/2.0/?format=json'
-    
+
+
 def _get_lastfm_nick(conn, irc_nick):
     nicks = db.get_row(conn, 'lastfm', 'irc_nick', irc_nick)
     try:
@@ -46,8 +47,7 @@ def _get_lastfm_nick(conn, irc_nick):
 def _add_lastfm_nick(conn, irc_nick, lastfm_nick):
     nick_check = None
     nick_check = _get_lastfm_nick(conn, irc_nick)
-    print(nick_check)
-    
+
     if nick_check is None:
         nickdata = (irc_nick, lastfm_nick)
         db.set_row(conn, 'lastfm', nickdata)
@@ -55,8 +55,9 @@ def _add_lastfm_nick(conn, irc_nick, lastfm_nick):
         db.set_cell(conn, 'lastfm',
                     'lastfm_nick', lastfm_nick,
                     'irc_nick', irc_nick)
-    
+
     return lastfm_nick
+
 
 def _pull_latest_track(api_key, lastfm_nick):
     url = api_url+f'&method=user.getrecenttracks'
@@ -69,6 +70,7 @@ def _pull_latest_track(api_key, lastfm_nick):
         except IndexError:
             pass
 
+
 def _pull_track_tags(api_key, track_title, track_artist):
     url = api_url+f'&method=track.gettoptags&track={track_title}'
     url += f'&artist={track_artist}&api_key={api_key}'
@@ -78,21 +80,21 @@ def _pull_track_tags(api_key, track_title, track_artist):
             tags_json = response.json()['toptags']['tag'][0:5]
         except IndexError:
             tags_json = response.json()['toptags']['tag']
-        
+
         if len(tags_json) < 1:
             return 'none'
-        
-        
+
         tags = []
         for tag in tags_json:
             tags.append(tag['name'])
         tags = ', '.join(tags)
         return tags
 
+
 def _get_when_played(track):
-    """returns the time difference between right now and the time the 
+    """returns the time difference between right now and the time the
     track was played in text format"""
-    
+
     """i decided to not include this in the output since the API
      is either buggy, or is reacting too slowly to changes. i.e a track
      i had just scrobbled shows up as 'played one hour ago' """
@@ -118,7 +120,7 @@ def _get_when_played(track):
             if listened[-8:-6] == ' 1' or listened[0:2] == '1:':
                 listened = listened[:-6] + ' hour'
             else:
-                listened = listened[:-6] + ' hours'    
+                listened = listened[:-6] + ' hours'
         return listened
     except KeyError:
         pass
@@ -126,7 +128,8 @@ def _get_when_played(track):
 
 def _pull_track_plays(api_key, track_title, track_artist, lastfm_nick):
     url = api_url+f'&method=track.getinfo&username={lastfm_nick}'
-    url += f'&track={track_title}&artist={track_artist}&api_key={api_key}'
+    url += f'&track={track_title}&artist={track_artist}'
+    url += f'&api_key={api_key}'
     response = requests.get(url)
     if response.status_code == 200:
         try:
@@ -134,21 +137,24 @@ def _pull_track_plays(api_key, track_title, track_artist, lastfm_nick):
             return str(playcount_json)
         except:
             return 'unknown'
-        
-def _output_nowplaying(client, data, lastfm_nick, title, artist, album, plays, tags):
+
+
+def _output_nowplaying(client, data, lastfm_nick,
+                       title, artist,album, plays, tags):
     out = f'{lastfm_nick} is listening to "{title}" by {artist} from '
     if album == 'an unknown album':
         out += 'an unknown album'
     else:
         out += f'the album {album}'
-    
+
     out += f', Play count: {plays}, Tags: {tags}.'
-    
+
     asyncio.create_task(
         client.message(
-            data.target, out))    
-    
+            data.target, out))
+
     return
+
 
 @hook.hook('command', ['lastfminit'], admin=True)
 async def lfminit(client, data):
@@ -158,14 +164,15 @@ async def lfminit(client, data):
     db.init_table(conn, 'lastfm', quote_columns)
     db.ccache()
     print('Initialization complete.')
-    
+
+
 @hook.hook('command', ['np'])
 async def nowplaying(client, data):
     """gets the current song that's playing."""
-            
+
     conn = client.bot.dbs[data.server]
     split = data.message.split()
-    
+
     try:
         api_keys = client.bot.config['api_keys']
         lastfm_key = api_keys['last_fm']
@@ -173,13 +180,13 @@ async def nowplaying(client, data):
         asyncio.create_task(
             client.message(
                 data.target,
-                'No last.fm API key found. Please create an API account, add it to \'api_keys\' in config.json with the tag \'last_fm\''))
+                'No last.fm API key found. Please create an API account, add it\'s API key to \'api_keys\' in config.json with the tag \'last_fm\''))
         return
-    
-    # dealing with getting the last.fm nickname
-    
+
+    # deal with getting the last.fm nickname
+
     lastfm_nick = None
-    
+
     if len(split) > 0:
         arg = split[0]
         if arg[0] == '@':
@@ -187,7 +194,7 @@ async def nowplaying(client, data):
             lastfm_nick = _get_lastfm_nick(conn, arg)
         else:
             lastfm_nick = _add_lastfm_nick(conn, data.nickname, arg)
-        
+
         if lastfm_nick is None:
             asyncio.create_task(
                 client.message(
@@ -196,42 +203,35 @@ async def nowplaying(client, data):
             return
     else:
         lastfm_nick = _get_lastfm_nick(conn, data.nickname)
-        
+
         if lastfm_nick is None:
             asyncio.create_task(
                 client.message(
                     data.target,
-                    'No associated last.fm nickname found.'))        
+                    'No associated last.fm nickname found.'))
             return
-    
+
     track = None
     track = _pull_latest_track(lastfm_key, lastfm_nick)
-    
+
     if track is None:
         asyncio.create_task(
             client.message(
                 data.target,
                 f'No recent tracks found for {lastfm_nick}.'))
         return
-    
-    #extract track data
+
+    # extract track data
     title = track['name']
     artist = track['artist']['#text']
     album = track['album']['#text']
     if album == '':
         album = 'an unknown album'
-    
-    #gather other data
+
+    # gather other data
     tags = _pull_track_tags(lastfm_key, title, artist)
     plays = _pull_track_plays(lastfm_key, title, artist, lastfm_nick)
-        
-    print(tags)
-    print(plays)
-    #print(when_played)
-    print(f'{title}, {artist}, {album}')
-    #final output
-    _output_nowplaying(client, data, lastfm_nick, title, artist, album, plays, tags)
 
-
-
-
+    # final output
+    _output_nowplaying(client, data, lastfm_nick,
+                       title, artist, album, plays, tags)
