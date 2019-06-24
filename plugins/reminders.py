462db465502e16fbdb17788559b2240b76dba2e9
koronis <nojusr@gmail.com>
2019-06-24 15:46:28 +0300

    added event listener for reminders; users can now view reminders.

--- plugins/reminders.py
+++ plugins/reminders.py
@@ -5,6 +5,8 @@
 #  .reminit                      -- initialize reminder db table
 #  .r [REMINDER_NAME] [MESSAGE]  -- add/append to a new reminder
 #  #[REMINDER_NAME]              -- view a reminder
+#                                   NOTE: can be changed by changing
+#                                   the value of remider_trigger_char
 
 from typing import List
 import asyncio
@@ -13,13 +15,19 @@ from datetime import datetime
 # import core db functions
 from core import db, hook
 
+# plugin config
+
+max_reminder_len = 180 # maximum length of reminders
+max_rem_name_len = 20
+reminder_trigger_char = '#' # chararcter to look for
+
+
+# db column def; dont touch
 reminder_columns: List[str] = [
     'name', 'msg', 'time_org', 'last_updated']
 
-max_reminder_len = 400 # maximum length of reminders
-
 def _get_reminder_text(conn, reminder_name):
-    #Finds reminder text if reminder has any
+    """Finds reminder text if reminder has any"""
     
     out = ''
     
@@ -34,7 +42,9 @@ def _get_reminder_text(conn, reminder_name):
 
 def _create_new_reminder(conn, rem_name, rem_text):
     """Is for creating new reminders in the db"""
-    rem_data = (rem_name, rem_text, str(int(time.time())), str(int(time.time())), )
+    
+    rem_text_trunc = rem_text[:max_reminder_len]
+    rem_data = (rem_name, rem_text_trunc, str(int(time.time())), str(int(time.time())), )
     db.set_row(conn, 'reminders', rem_data)
     db.ccache();
 
@@ -43,7 +53,10 @@ def _append_reminder(conn, rem_name, rem_text):
     old_rem_text = _get_reminder_text(conn, rem_name);
     new_rem_text = f'{old_rem_text} and {rem_text}'
     
-    db.set_cell(conn, 'reminders', 'msg', new_rem_text, 'name', rem_name)
+    rem_text_trunc = new_rem_text[:max_reminder_len]
+    
+    
+    db.set_cell(conn, 'reminders', 'msg', rem_text_trunc, 'name', rem_name)
     db.set_cell(conn, 'reminders', 'last_updated', str(int(time.time())), 'name', rem_name)
     db.ccache();
 
@@ -73,6 +86,7 @@ async def addrem(client, data):
     print ("REMINDER_DEBUG: running func")
 
     rem_name = split[0]
+    rem_name = rem_name[:max_rem_name_len]
     rem_user_text = ' '.join(split[1:])
     rem_text = _get_reminder_text(conn, rem_name)    
     
@@ -85,5 +99,14 @@ async def addrem(client, data):
         print(f'REMINDER_DEBUG: appending to reminder')
         _append_reminder(conn, rem_name, rem_user_text)
 
+@hook.hook('event', ['PRIVMSG'])
+async def remind(client, data):
+    """Is an event for posting reminders"""
+    conn = client.bot.dbs[data.server]
+    split = data.message.split()
     
+    if split[0][0] == reminder_trigger_char:
+        rem_name = split[0][1:]
+        rem_text = _get_reminder_text(conn, rem_name)
+        asyncio.create_task(client.message(data.target, f'{rem_name} {rem_text}'))
     
