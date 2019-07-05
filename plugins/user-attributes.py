4b0fe7f398e51c3121eb5d3c2f08c1994a44125e
koronis <nojusr@gmail.com>
2019-07-05 19:07:20 +0300

    added user attribute plugin

---
+++ plugins/user-attributes.py
@@ -0,0 +1,81 @@
+# user attribute plugin
+# author: nojusr
+#
+# usage:
+#  .homescreen @[IRC NICK]          -- check out what homescreen an
+#                                      user has
+#
+#  .homescreen                      -- show your current homescreen
+#
+#  .homescreen [TEXT]               -- set your current homescreen
+#  .homescreen del                  -- delete your current homescreen
+#
+#  note: the same usage and syntax applies for:
+#        .waifu, .desktop, .battlestation, .selfie, .husbando
+#  note: you can change the modifable user stats/attributes by adding 
+#        or removing values from usr_attributes
+
+from typing import List
+
+# import core db functions
+from core import db, hook
+
+usr_attributes: List[str] = ['homescreen', 'desktop', 'battlestation',
+                             'waifu', 'husbando', 'selfie']
+
+
+def _update_user_attribute(conn, attr_name, attr_value, username):
+    db.set_cell(conn, 'users', attr_name, attr_value, 'nick', username)
+    db.ccache()
+    return
+
+
+def _get_user_attribute(conn, attr_name, username):
+    attr = db.get_cell(conn, 'users', attr_name, 'nick', username)
+    
+    if attr is None or len(attr) == 0:
+        return f'No {attr_name} found for {username}.'
+    else:
+        return attr[0][0]
+
+
+@hook.hook('init', ['statinit'])
+async def statinit(client):
+    """Is used for initializing the database for this plugin"""
+    conn = client.bot.dbs[client.server_tag]
+    print(('Initializing stat columns in \'users\''
+           f' in /persist/db/{client.server_tag}.db...'))
+    for attr in usr_attributes:
+        db.add_column(conn, 'users', attr)
+    db.ccache()
+    print('User stat initialization complete.')
+
+
+@hook.hook('command', usr_attributes)
+async def stat(client, data):
+    conn = client.bot.dbs[data.server]
+    
+    message = data.split_message
+    command = data.command
+    
+    if len(message) < 1:
+        user_attr = _get_user_attribute(conn, data.command, 
+                                        data.nickname)
+        asyncio.create_task(client.message(data.target, user_attr))
+        return
+    
+    # check if user is trying to find another user's attribute
+    if message[0][0] == '@':
+        user_to_look_for = message[0][1:]
+        user_attr = _get_user_attribute(conn, data.command, 
+                                        user_to_look_for)
+        asyncio.create_task(client.message(data.target, user_attr))
+        return
+    
+    _update_user_attribute(conn, data.command, 
+                           data.message, data.nickname)
+    
+    asyncio.create_task(client.notice(
+                        data.target, 
+                        f'Updated {data.command} for {data.nickname}'))
+
