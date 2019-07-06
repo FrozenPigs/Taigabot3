0d0448200834620e3ae085ca9a24c2dcaf98bee3
adrift <676339784@users.noreply.github.com>
2019-07-06 01:44:24 -0700

    Style change for on connect show intro
    Renaming function

--- intro.py
+++ intro.py
@@ -81,7 +81,7 @@ async def intro(client, data):
     return
 
 @hook.hook('event', ['JOIN'])
-async def onconnectshowintro(client, data):
+async def on_connect_show_intro(client, data):
     conn = client.bot.dbs[data.server]
     intro_attr = _get_user_intro(conn, intro_name, data.nickname, True)
     if intro_attr is not None:
