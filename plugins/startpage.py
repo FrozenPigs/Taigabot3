bca60beb3956699a4e703ee1dad1a4abfa47138e
koronis <nojusr@gmail.com>
2019-07-06 23:05:12 +0300

    added an image search that still needs some work done to it.

--- plugins/startpage.py
+++ plugins/startpage.py
@@ -23,25 +23,82 @@ from datetime import datetime
 from core import db, hook
 
 startpage_link = 'https://www.startpage.com/do/search'
+is_gd_link = 'https://is.gd/create.php'
 
-@hook.hook('command', ['g', 'google', 'sp', 'startpage'])
+def _shorten_url(link):
+    """ Is used to shorten a link via is.gd"""
+    
+    #https://is.gd/create.php?format=simple&url=www.example.com.
+    req_data = {'format': 'simple', 'url': link }
+    r = requests.get(is_gd_link, req_data)
+    if r.status_code == 200:
+        return r.text
+    else:
+        print(f'SEARCH_DEBUG: is.gd fail: {r.status_code}, {r.text}')
+        return f'Failed to shorten link: is.gd returned {r.status_code}'
+
+
+def _get_first_result(req_data):
+    """Is used to get the first text result from search"""
+    r = requests.get(startpage_link, req_data)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    
+    first_result = soup.find('li', class_=['search-result','search-item'])
+    
+    return first_result
+
+def _get_first_img_result(req_data):
+    
+    r = requests.get(startpage_link, req_data)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    
+    first_result = soup.find('li', id='image-0')
+    
+    
+    return first_result
+
+
+@hook.hook('command', ['g', 'google'])
 async def startpage_search(client, data):
     split = data.split_message
     
     req_data = {'query': ' '.join(split)}
-
-    r = requests.get(startpage_link, req_data)
     
-    soup = BeautifulSoup(r.text, 'html.parser')
     
-    first_result = soup.find('li', class_=['search-result','search-item'] limit=1)
+    first_result = _get_first_result(req_data)
+    
+    while first_result is None:
+        print('SEARCH_DEBUG: failed to get first result, making another request')
+        first_result = _get_first_result(req_data)
+        await asyncio.sleep(0.5)
+    
+    
     title = first_result.find('h3', class_='search-item__title').find('a')
-    print(title.attrs['href'])
     link = title.attrs['href']
-    print(title.text)
+    
+    short_link = _shorten_url(link)
+    
     body = first_result.find('p', class_='search-item__body')
-    print(body.text)
-    output = f'{link} -- {title.text}: \"{body.text}\"'
+    output = f'{short_link} -- {title.text}: \"{body.text}\"'
     
     asyncio.create_task(client.message(data.target, output))
 
+
+@hook.hook('command', ['gi'])
+async def startpage_img_search(client, data):
+    split = data.split_message
+    
+    req_data = {'cat': 'pics', 'query': ' '.join(split)}
+    
+    first_result = _get_first_img_result(req_data)
+    
+    while first_result is None:
+        print('SEARCH_DEBUG: failed to get first result for image, making another request')
+        first_result = _get_first_img_result(req_data)
+
+
+    a = first_result.find('a', class_='image-result__overlay-image')
+    link = a.attrs['href']
+
+    short_link = _shorten_url(link)
+    asyncio.create_task(client.message(data.target, short_link))
