9b26df958e86da2f60c50d3946b173f025d6bd41
676339784 <676339784@qq.com>
2019-07-10 14:17:05 -0700

    Query for examples, misc changes

--- plugins/urban.py
+++ plugins/urban.py
@@ -7,7 +7,6 @@
 from core import hook
 
 import json
-import re
 import urllib
 
 @hook.hook('command', ['ud', 'urban'])
@@ -16,11 +15,15 @@ async def urban(client, data):
     # sets the index for which definition to query
     definition_index = 0
 
-    max_response_length = 400
+    max_response_length = 405
 
     query = ''
     split = data.split_message
-    
+
+    # empty query
+    if not split:
+        return
+     
     # if user is specifiying a definition index specific parsing is required
     if split[-1].isnumeric():
         # query should not have last word, which indicates definition index
@@ -36,11 +39,16 @@ async def urban(client, data):
     try:
         definitions = urllib.request.urlopen(request_url)
     except urllib.error.HTTPError:
-        asyncio.create_task(client.message(data.target, "No definitions available."))
+        asyncio.create_task(client.message(data.target, "HTTP error."))
         return
 
     definitions = urllib.request.urlopen(request_url)
     j_definitions = json.load(definitions)
+
+    # no definitions
+    if not j_definitions["list"]:
+        asyncio.create_task(client.message(data.target, "No definitions available."))
+        return
 
     # try to see if request definition index is out of range
     try:
@@ -51,11 +59,12 @@ async def urban(client, data):
 
     definition_list = j_definitions["list"][definition_index]
     definition = definition_list["definition"].replace('\n', ' ')
+    example = definition_list["example"].replace('\n', ' ')
     definition_no_brackets = definition.replace('[', '').replace(']', '')
-    print(definition_no_brackets)
+    example_no_brackets = example.replace('[', '').replace(']', '')
 
-    # api caps at 10 definitions
-    urban_response = "[" + str(definition_index) + "/10] " + "\02" + definition_list["word"] + ": \02" + definition_no_brackets
+    # api caps at 10 definitions, which is why 10 is hardcoded
+    urban_response = "[" + str(definition_index) + "/10] " + "\02" + definition_list["word"] + ": \02" + definition_no_brackets + " " + example_no_brackets
 
     # trim response length as necessary
     if len(urban_response) > max_response_length:
