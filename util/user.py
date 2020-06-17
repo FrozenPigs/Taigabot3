df83e5f96cbea4d74b9328142127a926ff5d83e5
FrozenPigs <frozenpigs@cock.li>
2020-06-17 14:06:07 +0100

    Convert user.py to the new core
    (mostly untested)

--- util/user.py
+++ util/user.py
@@ -7,13 +7,13 @@ from typing import Any, Dict, List, Optional
 from core import db
 
 
-async def is_gadmin(client: Any, server: str, mask: str) -> bool:
+async def is_gadmin(bot: Any, server: str, mask: str) -> bool:
     """Is used to check if mask is a gadmin on the server."""
-    admins = client.bot.config['servers'][server]['admins']
+    admins = bot.server_config.admins
     return all(mask == nmask for nmask in admins)
 
 
-async def is_admin(client: Any, conn: Connection, nick: str, mask: str) -> bool:
+async def is_admin(bot: Any, conn: Connection, nick: str, mask: str) -> bool:
     """Is used to check if a mask is an admin in the channel."""
     db.add_column(conn, 'channels', 'admins')
     admins = db.get_cell(conn, 'channels', 'admins', 'channel', nick)
@@ -25,7 +25,7 @@ async def is_admin(client: Any, conn: Connection, nick: str, mask: str) -> bool:
     return False
 
 
-async def is_ignored(client: Any, conn: Connection, target: str, mask: str) -> bool:
+async def is_ignored(bot: Any, conn: Connection, target: str, mask: str) -> bool:
     """Is used to check if a mask is ignored in a channel."""
     db.add_column(conn, 'channels', 'ignored')
     ignores = db.get_cell(conn, 'channels', 'ignored', 'channel', target)
@@ -37,25 +37,25 @@ async def is_ignored(client: Any, conn: Connection, target: str, mask: str) -> b
     return False
 
 
-async def is_gignored(client: Any, server: str, mask: str) -> bool:
+async def is_gignored(bot: Any, server: str, mask: str) -> bool:
     """Is used to check if a mask is gignored on the server."""
-    ignored = client.bot.config['servers'][server]['ignored']
+    ignored = bot.server_config.ignored
     return all(mask == nmask for nmask in ignored)
 
 
-async def is_user(client: Any, conn: Connection, nick: str) -> bool:
+async def is_user(bot: Any, conn: Connection, nick: str) -> bool:
     """Is used to determain if a user is in db or user dict."""
     db_user = db.get_cell(conn, 'users', 'mask', 'nick', nick)
-    if nick not in client.users and not db_user:
+    if nick not in bot.users and not db_user:
         return False
     return True
 
 
-async def get_mask(client: Any, conn: Connection, nick: str) -> str:
+async def get_mask(bot: Any, conn: Connection, nick: str) -> str:
     """Is used to combine user information into a mask."""
     user: Dict[str, str]
     try:
-        user = client.users[nick]
+        user = bot.users[nick]
     except KeyError:
         db_user = db.get_cell(conn, 'users', 'mask', 'nick', nick)
         if db_user:
@@ -66,7 +66,7 @@ async def get_mask(client: Any, conn: Connection, nick: str) -> str:
     return mask
 
 
-async def parse_masks(client: Any, conn: Connection, inp: str) -> List[str]:
+async def parse_masks(bot: Any, conn: Connection, inp: str) -> List[str]:
     """Is used for parsing space seperated lists of users into masks."""
     split_inp: List[str]
     if ' ' in inp:
@@ -75,8 +75,7 @@ async def parse_masks(client: Any, conn: Connection, inp: str) -> List[str]:
         split_inp = [inp]
     print(inp)
     masks: List[str] = [
-        await get_mask(client, conn, user) for user in split_inp if await is_user(
-            client, conn, user)
+        await get_mask(bot, conn, user) for user in split_inp if await is_user(bot, conn, user)
     ]
     masks.extend(mask for mask in split_inp if '@' in mask)
     return masks
